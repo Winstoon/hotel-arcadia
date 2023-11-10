81c2e9a77ced03def8058446ba30fc5a068ecf0e
hewb <hewb@trip.com>
2023-11-10 08:56:29 +0800

    + upgrade pics.

--- src/pages/Pics/index.tsx
+++ src/pages/Pics/index.tsx
@@ -6,21 +6,21 @@ import './index.css'
 import Footer from "../../components/Footer";
 
 const data = [
-    { id: 1, url: '/jpgs/pic-1.jpg', burl: '/jpgs/bpic-1.jpg' },
-    { id: 2, url: '/jpgs/pic-2.jpg', burl: '/jpgs/bpic-2.jpg' },
-    { id: 3, url: '/jpgs/pic-3.jpg', burl: '/jpgs/bpic-3.jpg' },
-    { id: 4, url: '/jpgs/pic-4.jpg', burl: '/jpgs/bpic-4.jpg' },
-    { id: 5, url: '/jpgs/pic-5.jpg', burl: '/jpgs/bpic-5.jpg' },
-    { id: 6, url: '/jpgs/pic-6.jpg', burl: '/jpgs/bpic-6.jpg' },
-    { id: 7, url: '/jpgs/pic-7.jpg', burl: '/jpgs/bpic-7.jpg' },
-    { id: 8, url: '/jpgs/pic-8.jpg', burl: '/jpgs/bpic-8.jpg' },
-    { id: 9, url: '/jpgs/pic-9.jpg', burl: '/jpgs/bpic-9.jpg' },
-    { id: 10, url: '/jpgs/pic-10.jpg', burl: '/jpgs/bpic-10.jpg' },
-    { id: 11, url: '/jpgs/pic-11.jpg', burl: '/jpgs/bpic-11.jpg' },
-    { id: 12, url: '/jpgs/pic-12.jpg', burl: '/jpgs/bpic-12.jpg' },
-    { id: 13, url: '/jpgs/pic-13.jpg', burl: '/jpgs/bpic-13.jpg' },
-    { id: 14, url: '/jpgs/pic-14.jpg', burl: '/jpgs/bpic-14.jpg' },
-    { id: 15, url: '/jpgs/pic-15.jpg', burl: '/jpgs/bpic-15.jpg' }
+    { id: 1, url: '/jpgs/pic-1.jpg', burl: '/pngs/bpic-1.png' },
+    { id: 2, url: '/jpgs/pic-2.jpg', burl: '/pngs/bpic-2.png' },
+    { id: 3, url: '/jpgs/pic-3.jpg', burl: '/pngs/bpic-3.png' },
+    { id: 4, url: '/jpgs/pic-4.jpg', burl: '/pngs/bpic-4.png' },
+    { id: 5, url: '/jpgs/pic-5.jpg', burl: '/pngs/bpic-5.png' },
+    { id: 6, url: '/jpgs/pic-6.jpg', burl: '/pngs/bpic-6.png' },
+    { id: 7, url: '/jpgs/pic-7.jpg', burl: '/pngs/bpic-7.png' },
+    { id: 8, url: '/jpgs/pic-8.jpg', burl: '/pngs/bpic-8.png' },
+    { id: 9, url: '/jpgs/pic-9.jpg', burl: '/pngs/bpic-9.png' },
+    { id: 10, url: '/jpgs/pic-10.jpg', burl: '/pngs/bpic-10.png' },
+    { id: 11, url: '/jpgs/pic-11.jpg', burl: '/pngs/bpic-11.png' },
+    { id: 12, url: '/jpgs/pic-12.jpg', burl: '/pngs/bpic-12.png' },
+    { id: 13, url: '/jpgs/pic-13.jpg', burl: '/pngs/bpic-13.png' },
+    { id: 14, url: '/jpgs/pic-14.jpg', burl: '/pngs/bpic-14.png' },
+    { id: 15, url: '/jpgs/pic-15.jpg', burl: '/pngs/bpic-15.png' },
 ]
 
 function PPT ({
@@ -61,9 +61,11 @@ function PPT ({
             <div className={`ppt-btn ${prevable ? '': 'disabled'}`} onClick={handlePrev}>
                 <Image src="/icons/prev.svg" />
             </div>
-            <div className="img-box">
+            <div className="ppt-imgs">
                 {images.map((img, index) =>
-                    <Image className={index === idx ? 'active' : ''} src={img.burl} />
+                    <div className={`ppt-item ${index === idx ? 'active' : ''}`}>
+                        <Image src={img.burl} />
+                    </div>
                 )}
             </div>
             <div className={`ppt-btn ${nextable ? '': 'disabled'}`} onClick={handleNext}>
